554c3bed00cc5ce69c7677fc8533ba3394ea6881
alexhein189 <zcabhei@ucl.ac.uk>
2020-05-24 18:02:10 +0630

    without testing code

--- volunteer.py
+++ volunteer.py
@@ -11,11 +11,3 @@ class volunteer:
         self.private_id_code = id_code  # int
         self.timestamp = timestamp
         self.available_location = available_location
-
-
-# avaiable_time_a = ["Tuesdays", "Thursdays"], ["08:00-09:00","09:00-10:00","10:00-11:00","11:00-12:00","12:00-1:00"]
-available_time_of_vol_a = available_time(
-    {"Tuesdays": ["08:00-09:00", "09:00-10:00", "10:00-11:00"], "Thursdays": ["11:00-12:00", "12:00-1:00"]})
-available_location_of_vol_a = address_availablity((213.23, 12.112), 5)
-volunteer_A = volunteer("Alex", "getitboy12", 1231, available_time_of_vol_a, available_location_of_vol_a)
-print("hello")
