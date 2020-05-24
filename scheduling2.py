543acea04a823c4a13de2fc76dfa6a304e255c43
alexhein189 <zcabhei@ucl.ac.uk>
2020-05-24 17:59:39 +0630

    changing the data structure to dictionary of lists

--- scheduling2.py
+++ scheduling2.py
@@ -1,7 +1,6 @@
-from typing import List
+from typing import List, Dict
 
 
 class available_time:
-    def __init__(self, days: List[str], hour: List[str]):
-        self.days = days
-        self.hour = hour
+    def __init__(self, timestamp: Dict[str,List[str]]):
+        self.timestamp = timestamp
--- volunteer.py
+++ volunteer.py
@@ -1,20 +1,21 @@
 from address_availablity import address_availablity
 from scheduling2 import available_time
 
+
 class volunteer:
 
-    def __init__(self, name:str, username:str, id_code:int, timestamp:available_time, available_location:address_availablity):
+    def __init__(self, name: str, username: str, id_code: int, timestamp: available_time,
+                 available_location: address_availablity):
         self.name = name
-        self.public_username = username #string
-        self.private_id_code = id_code #int
+        self.public_username = username  # string
+        self.private_id_code = id_code  # int
         self.timestamp = timestamp
         self.available_location = available_location
 
 
-available_time_of_vol_a = available_time(["Tuesdays", "Thursdays"], ["08:00-09:00","09:00-10:00","10:00-11:00","12:00-1:00"])
-available_location_of_vol_a = address_availablity((213.23,12.112),5)
+# avaiable_time_a = ["Tuesdays", "Thursdays"], ["08:00-09:00","09:00-10:00","10:00-11:00","11:00-12:00","12:00-1:00"]
+available_time_of_vol_a = available_time(
+    {"Tuesdays": ["08:00-09:00", "09:00-10:00", "10:00-11:00"], "Thursdays": ["11:00-12:00", "12:00-1:00"]})
+available_location_of_vol_a = address_availablity((213.23, 12.112), 5)
 volunteer_A = volunteer("Alex", "getitboy12", 1231, available_time_of_vol_a, available_location_of_vol_a)
 print("hello")
-
-
-
