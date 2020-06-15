671cc0c5d497049c0e0c29b25e8a358e1bee8a67
alexhein189 <zcabhei@ucl.ac.uk>
2020-06-16 04:41:36 +0630

    half-way through implementing first version of optimal combination method

--- items_request.py
+++ items_request.py
@@ -1,5 +1,5 @@
 #when vulnerable person request some items
-from typing import List
+from typing import List, Dict
 
 import datetime
 
@@ -10,7 +10,9 @@ class itemsRequest:
         self.vulnerable_id_code = vulnerable_id_code
         self.start_date = start_date
         self.end_date = end_date
-        self.priority_hours = ((end_date - start_date).total_seconds() // 3600)  #time
+        self.main_dictionary= main_dictionary
+        self.priority_hours = int(((end_date - start_date).total_seconds() // 3600))#time
+        self.difference_in_days = (self.priority_hours) // 24
         self.nearest_volunteers = main_dictionary[vulnerable_id_code].nearest_volunteers
         self.emergency = emergency
         self.number_of_shops = number_of_shops
@@ -26,7 +28,55 @@ class itemsRequest:
         return priority_score
 
     def find_volunteers_with_time(self):
-        pass
+        list_of_ids = []
+        for i in range(self.difference_in_days):
+            for id in self.nearest_volunteers:
+                if (self.start_date.weekday()+i) % 7 in self.main_dictionary[id].timestamp:
+                    if id not in list_of_ids:
+                        list_of_ids.append(id)
+        return list_of_ids
+
+    def finding_shops_with_items(self, list_of_ids):
+        dictionary_of_shops ={}
+        dictionary_of_items = {}
+        for id in list_of_ids:
+            for shop_id in self.main_dictionary[id].nearest_shops:
+                list_of_items =[]
+                for item in self.items:
+                    if item in self.main_dictionary[shop_id].items_in_stock:
+                        list_of_items.append(item)
+                        if item not in dictionary_of_items:
+                            dictionary_of_items[item] = []
+                        dictionary_of_items[item].append(shop_id)
+                if len(list_of_items) >= 1:
+                    dictionary_of_shops[shop_id] = list_of_items
+        return dictionary_of_shops, dictionary_of_items
+
+    def finding_optimal_combination(self, dictionary_of_user, dictionary_of_items):
+        list_of_optimal_users = []
+        for _ in dictionary_of_user:
+            min_number_of_items = len(self.items) + 1
+            user_id_with_min_number_of_items = None
+            for user_id in dictionary_of_user:
+                if user_id in list_of_optimal_users:
+                    continue
+                number_of_items = len(dictionary_of_user[user_id])
+                if number_of_items < min_number_of_items:
+                    min_number_of_items = number_of_items
+                    user_id_with_min_number_of_items = user_id
+
+            counter = 0
+            for item in dictionary_of_user[user_id_with_min_number_of_items]:
+                if len(dictionary_of_items[item]) > 1:
+                    counter +=1
+            if counter == len(dictionary_of_user[user_id_with_min_number_of_items]):
+                del dictionary_of_user[user_id_with_min_number_of_items]
+            else:
+                list_of_optimal_users.append(user_id_with_min_number_of_items)
+
+
+
+
 
 
 
